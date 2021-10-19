f5507f15b52c01bd01624405b9fdb2bee8090aac
Ganreshnu <ganreshnu@gmail.com>
2021-10-19 17:29:51 -0500

    Adding the lua language server config.

--- ftplugin/lua.lua
+++ ftplugin/lua.lua
@@ -4,3 +4,10 @@ vim.bo.shiftwidth = 2
 vim.bo.softtabstop = 0
 vim.bo.expandtab = false
 
+use { 'neovim/nvim-lspconfig',
+	config = function()
+		require('lspconfig').sumneko_lua.setup({
+
+		})
+	end
+}
